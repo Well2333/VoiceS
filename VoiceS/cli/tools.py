4ddb75ccb45b7d1ba262aa1bfb61516c1fe0855f
Well404 <well_404@outlook.com>
2023-03-17 13:54:47 +0800

    1.1.0 升级uta的功能

--- VoiceS/cli/tools.py
+++ VoiceS/cli/tools.py
@@ -18,6 +18,22 @@ from noneprompt import CancelledError
     help="处理后输出的文件",
 )
 @click.option(
+    "-t",
+    "--track",
+    default=None,
+    prompt="请输入需要提取的轨道名",
+    prompt_required=False,
+    help="需要提取的轨道",
+)
+@click.option(
+    "-l",
+    "--lyrics",
+    default=None,
+    prompt="请输入歌词原文文件",
+    prompt_required=False,
+    help="歌词原文文件",
+)
+@click.option(
     "--perset",
     default=0,
     prompt="请输入每行前添加的空白秒数, 单位为毫秒(ms)",
@@ -31,15 +47,22 @@ from noneprompt import CancelledError
     prompt_required=False,
     help="每行后添加的空白秒数",
 )
-def uta(input: str, output: str, perset: int, offset: int):
+def uta(input: str, output: str, track: str, lyrics: str, perset: int, offset: int):
     # sourcery skip: raise-from-previous-error
     src = Path(input)
     des = Path(output)
+    lys = Path(lyrics).read_text("utf-8") if lyrics else ""
 
     try:
         from ..functions.tools.uta import UtA
 
         click.secho(f"正在将 {src.absolute()} 转化为 {des.absolute()}")
-        UtA(src.read_text("utf-8"), perset=perset, offset=offset).write(des)
+        UtA(
+            src.read_text("utf-8"),
+            track,
+            lys,
+            perset=perset,
+            offset=offset,
+        ).write(des)
     except CancelledError:
         raise KeyboardInterrupt
--- VoiceS/functions/tools/uta.py
+++ VoiceS/functions/tools/uta.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from typing import List
+from typing import List, Union
 
 import yaml
+import sys
 
 
 def get_time(ms: int):
@@ -13,11 +14,15 @@ def get_time(ms: int):
 
 class Note:
     def __init__(
-        self, position: int, duration: int, lyric: str, mspt: float = 1
+        self,
+        position: Union[float, int],
+        duration: Union[float, int],
+        lyric: str,
+        mspt: Union[float, int] = 1,
     ) -> None:
-        self.start: float = position * mspt
-        self.dur: float = duration * mspt
-        self.end: float = self.start + self.dur
+        self.start: Union[float, int] = position * mspt
+        self.dur: Union[float, int] = duration * mspt
+        self.end: Union[float, int] = self.start + self.dur
         self.lyric: str = " " if lyric in {"AP", "SP"} else lyric
 
     def __add__(self, other):
@@ -27,74 +32,151 @@ class Note:
 
 class UtA:
     def __init__(
-        self, src_proj: str, src_text: str = "", perset: int = 0, offset: int = 0
+        self,
+        src_proj: str,
+        track_name: str = "",
+        lyrics: str = "",
+        perset: int = 0,
+        offset: int = 0,
     ):
+        """Generate ASS subtitle files
+
+        Args:
+            src_proj (str): [ustx file]
+            track_name (str, optional): [ustx track name]. Defaults to None.
+            lyrics (str, optional): [lyrics file]. Defaults to "".
+            perset (int, optional): [perset of line]. Defaults to 0.
+            offset (int, optional): [offset of line]. Defaults to 0.
+        """ """"""
         self.src = yaml.load(src_proj, Loader=yaml.FullLoader)
-        self.src_text = src_text
         self.perset = perset
         self.offset = offset
+        self.track_name = track_name
+
+        self.perfix = self.get_k_word(int(self.perset // 10), "") if self.perset else ""
+        self.suffix = self.get_k_word(int(self.offset // 10), "") if self.offset else ""
+        self.mspt = 60000 / (self.src["bpm"] * self.src["resolution"])
 
         self.out = []
         self.index = -1
-        self.mspt = 60000 / (self.src["bpm"] * self.src["resolution"])
         self.get_tracks()
-        self.get_ass_line()
+        self.get_lyrics_line(lyrics) if lyrics else self.get_ustx_line()
 
     @staticmethod
-    def get_time(ms: int):
+    def get_time(ms: Union[float, int]):
         s, ms = divmod(ms, 1000)
         m, s = divmod(s, 60)
         h, m = divmod(m, 60)
         return "%01d:%02d:%02d.%02d" % (h, m, s, ms // 10)
 
+    @staticmethod
+    def get_k_word(ktime: Union[int, str], word: str):
+        return r"{\k" + str(ktime) + "}" + word
+
     def get_tracks(self):
-        self.tracks: dict[str, List[Note]] = {}
-
         for track in self.src["voice_parts"]:
-            notes: List[Note] = []
-            for raw_note in track["notes"]:
-                note = Note(
-                    raw_note["position"],
-                    raw_note["duration"],
-                    raw_note["lyric"],
-                    self.mspt,
-                )
-                if note.lyric in ["+"]:
-                    note = notes.pop() + note
-                notes.append(note)
-            self.tracks[track["name"]] = notes
-
-    def get_ass_line(self):
-        perfix = "{\k" + str(int(self.perset // 10)) + "} " if self.perset else ""
-        suffix = "{\k" + str(int(self.offset // 10)) + "} " if self.offset else ""
-        for track_name in self.tracks.keys():
-            notes = self.tracks[track_name]
-            text = perfix
-            start = 0
-            for index, note in enumerate(notes):
-                # check is empty
-                if note.lyric == " " and text == perfix:
-                    continue
-                # get start time
-                if not start:
-                    start = note.start - self.perset
-                # check is new line
-                if (
-                    index > 0
-                    and note.start > notes[index - 1].end + 1  # prevent float error
-                ):
-                    self.out.append(
-                        f"Dialogue: 0,{self.get_time(start)},{self.get_time(notes[index-1].end+self.offset)},Default,{track_name},0,0,0,,{text+suffix}"
-                    )
-                    start = note.start - self.perset
-                    text = perfix
-                    if note.lyric == " ":
-                        start = 0
-                        continue
-                text += "{\k" + str(int(note.dur // 10)) + "}" + note.lyric
-            self.out.append(
-                f"Dialogue: 0,{self.get_time(start)},{self.get_time(note.end+self.offset)},Default,{track_name},0,0,0,,{text+suffix}"
+            if (self.track_name and track["name"] == self.track_name) or (
+                not self.track_name
+            ):
+                break
+        else:
+            print(f"There is no track named {self.track_name}")
+            sys.exit(1)
+        notes: List[Note] = []
+        for raw_note in track["notes"]:
+            note = Note(
+                raw_note["position"],
+                raw_note["duration"],
+                raw_note["lyric"],
+                self.mspt,
             )
+            if note.lyric in ["+"]:
+                note = notes.pop() + note
+            notes.append(note)
+        self.notes = notes
+
+    def get_ustx_line(self):
+        """Generate subtitles directly from ustx files"""
+        text = self.perfix
+        start = 0
+        note = self.notes[0]
+        for index, note in enumerate(self.notes):
+            # check is empty
+            if note.lyric == " " and text == self.perfix:
+                continue
+            # get start time
+            if not start:
+                start = note.start - self.perset
+            # check is new line
+            if (
+                index > 0
+                and note.start > self.notes[index - 1].end + 1  # prevent float error
+            ):
+                self.out.append(
+                    f"Dialogue: 0,{self.get_time(start)},{self.get_time(self.notes[index-1].end+self.offset)},Default,{self.track_name},0,0,0,,{text+self.suffix}"
+                )
+                start = note.start - self.perset
+                text = self.perfix
+                if note.lyric == " ":
+                    start = 0
+                    continue
+            text += self.get_k_word(int(note.dur // 10), note.lyric)
+        self.out.append(
+            f"Dialogue: 0,{self.get_time(start)},{self.get_time(note.end+self.offset)},Default,{self.track_name},0,0,0,,{text+self.suffix}"
+        )
+
+    @staticmethod
+    def _lyrics_gener(lyrics: str):
+        for w in lyrics:
+            if w in [" ", ",", "，", ".", "。", ";", "；"]:
+                continue
+            yield w
+
+    def get_lyrics_line(self, lyrics: str):
+        """Generate subtitles from lyrics files and mark the time using ustx files"""
+        lyrics_gen = self._lyrics_gener(lyrics)
+        text = self.perfix
+        start = 0
+        end = 0
+        for index, note in enumerate(self.notes):
+            # check is empty
+            if note.lyric == " ":
+                if text == self.perfix:
+                    continue
+                word = " "
+            else:
+                word = next(lyrics_gen)
+            # get start time
+            if not start:
+                start = note.start - self.perset
+            # fill break time
+            if (
+                index > 0
+                and text != self.perfix
+                and note.start > self.notes[index - 1].end + 1  # prevent float error
+            ):
+                text += self.get_k_word(
+                    int((note.start - self.notes[index - 1].end) // 10), ""
+                )
+            # check is new line
+            if word == "\n":
+                text = text.rstrip(r"{}\k1234567890 ")
+                self.out.append(
+                    f"Dialogue: 0,{self.get_time(start)},{self.get_time(end)},Default,{self.track_name},0,0,0,,{text+self.suffix}"
+                )
+                start = note.start - self.perset
+                text = self.perfix
+                if note.lyric == " ":
+                    start = 0
+                    continue
+                word = next(lyrics_gen)
+
+            text += self.get_k_word(int(note.dur // 10), word)
+            end = note.end + self.offset if word != " " else end
+        text = text.rstrip(r"{}\k1234567890 ")
+        self.out.append(
+            f"Dialogue: 0,{self.get_time(start)},{self.get_time(end)},Default,{self.track_name},0,0,0,,{text+self.suffix}"
+        )
 
     def write(self, des: Path):
         des.parent.mkdir(0o755, parents=True, exist_ok=True)
@@ -120,6 +202,7 @@ class UtA:
 
 # just for test
 # UtA(
-#    Path("能解答一切的答案.ustx").read_text("utf-8"),
-#    perset=150
-# ).write(Path("能解答一切的答案.ass"))
+#     Path(r"C:\Users\Well404\Desktop\铃芽之旅.ustx").read_text("utf-8"),
+#     lyrics = Path(r"C:\Users\Well404\Desktop\铃芽之旅.txt").read_text("utf-8"),
+#     perset=150
+# ).write(Path(r"C:\Users\Well404\Desktop\铃芽之旅.ass"))
